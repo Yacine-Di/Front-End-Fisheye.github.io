4f93fd212d676d3ebc8bfa9666bae7474b263b47
Yacine Diaf <yacinediafwork@gmail.com>
2023-12-26 14:33:52 +0100

    recuperation des dataet affichage des photograpghes

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,26 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
-
+    
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        
+        const link = document.createElement('a')
+        link.href = "#"
+        link.appendChild(img);
+        link.appendChild(h2);
+
+        const p = document.createElement('p');
+        p.textContent = `${city}, ${country}`
+        
+        article.appendChild(link);
+        article.appendChild(p)
         return (article);
     }
     return { name, picture, getUserCardDOM }
