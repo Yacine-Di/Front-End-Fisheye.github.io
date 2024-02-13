dd56d269d3dbd1729a298ce63d4271cc5cabe57c
Yacine Diaf <yacinediafwork@gmail.com>
2024-02-13 17:35:23 +0100

    modifs

--- scripts/templates/sorterForm.js
+++ scripts/templates/sorterForm.js
@@ -9,7 +9,33 @@ class SorterForm{
         this.mediaWrapper = document.querySelector('.medias-wrapper')
     }
 
+    sorterMedias(sorter){
+        this.clearMediasWrapper()
+        let sortedMedias = []
+        let mediaIndex = 0
 
+        if(sorter === 'POPULARITY'){
+            sortedMedias = Array.from(this._medias).sort((a,b) => b.likes - a.likes)
+        } else if(sorter === 'DATE'){
+            sortedMedias = Array.from(this._medias).sort((a,b) => new Date(a.date) - new Date(b.date))
+        } else if(sorter === 'TITLE'){
+            sortedMedias = Array.from(this._medias).sort((a,b) => a.title.localeCompare(b.title))
+        }
+
+        sortedMedias.forEach(media =>{
+            if('image' in media){
+                let photo = new MediaFactory(media, 'image')
+                const photoTemplate = new PhotoTemplate(photo, this.mediaWrapper, mediaIndex)
+                photoTemplate.displayPhotoTemplate()
+                mediaIndex++
+             } else{
+                let video = new MediaFactory(media, 'video')
+                const videoTemplate = new VideoTemplate(video, this.mediaWrapper, mediaIndex)
+                videoTemplate.displayVideoTemplate()
+                mediaIndex++
+             }
+        })
+    }
 
     /**
      * Gère l'évènement du formulaire
@@ -20,16 +46,22 @@ class SorterForm{
             this.sorterMedias(sorter)
         })
     }
+
+    clearMediasWrapper(){
+        this.mediaWrapper.innerHTML = ""
+    }
     /**
      * Créé le formulaire de selection
      */
     createSorterForm(){
         const sorterFrom = `
             <form action="#" method="POST" class="sorter-form">
-                <label for="sorter-select">Triez par date de sortie : </label>
+                <label for="sorter-select">Triez par</label>
                 <select name="sorter-select" id="sorter-select">
-                    <option value="">Popularité</option>
+                    <option value="POPULARITY">Popularité</option>
+                    <option class="divider" disabled></option>
                     <option value="DATE">Date</option>
+                    <option class="divider" disabled></option>
                     <option value="TITLE">Titre</option>
                 </select>
             </form>
